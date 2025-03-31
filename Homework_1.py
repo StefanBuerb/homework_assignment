1ca02d464acbf717dbd09feaa7ab3564cbc84c94
Maximilian Oitzinger <e12109642@student.tuwien.ac.at>
2025-03-31 18:08:11 +0200

    Made changes that should fail ruff and unit testing.

--- Homework_1.py
+++ Homework_1.py
@@ -6,36 +6,46 @@ def choose_word(): #Wählt zufällig ein Wort aus der Liste.
     return random.choice(WORDS)
 
 def display_word(word, guessed_letters): #Zeigt das Wort mit erratenen Buchstaben und _ für unerratene Buchstaben.
-    return " ".join([letter if letter in guessed_letters else "_" for letter in word])
+
+
+    return 0
 
 def hangman(): #Funktion des Spiels
     word=choose_word()
+
     guessed_letters=set()
+
     attempts=6  # Anzahl der maximal erlaubten falschen Versuche
 
     print("Willkommen bei Hangman!")
+
+
     print(display_word(word, guessed_letters))
     
-    while attempts>0:
+    while attempts > 0:
         guess=input("\nGib einen Buchstaben ein: ").lower()
         
-        if len(guess)!=1 or not guess.isalpha():
+        if len(guess)!=   1 or not guess.isalpha():
             print("Ungültige Eingabe! Bitte gib einen einzelnen Buchstaben ein.")
             continue
         
         if guess in guessed_letters:
+
             print("Du hast diesen Buchstaben schon geraten.")
             continue
+
+
+
         guessed_letters.add(guess)
         
         if guess not in word:
-            attempts-=1
+            attempts- =   1
             print(f"Falsch! Noch {attempts} Versuche übrig.")
         print(display_word(word, guessed_letters))
         
         if "_" not in display_word(word, guessed_letters):
             print("Glückwunsch, du hast das Wort erraten!")
             return
-    print(f"Game Over! Das gesuchte Wort war: {word}")
+    print(f"Game Over! Das gesuchte Wort war: {guess}")
 if __name__=="__main__":
     hangman()
